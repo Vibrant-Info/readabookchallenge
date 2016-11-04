ce74cf3a86a308e768c7e250de798fd1a09c1566
lingeswrn <lingeswrn@gmail.com>
2016-11-04 18:18:10 +0530

    twitter login

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,15 @@
 
 // app/routes.js
-
+var OAuth = require('oauth').OAuth
+  , oauth = new OAuth(
+      "https://api.twitter.com/oauth/request_token",
+      "https://api.twitter.com/oauth/access_token",
+      "hXexltZCLpO49bLRwxp4wNV0T",
+      "NwYl2EB1nUqq9qHY121E20wKtYGBUyo1qWTrsF2XQGRvRlx6YJ",
+      "1.0",
+      "http://localhost:3000/auth/twitter/callback",
+      "HMAC-SHA1"
+    );
 module.exports = function(app, passport, connection) {   
     app.get('/logout', function(req, res) {
 		req.session.destroy();
@@ -61,6 +70,59 @@ module.exports = function(app, passport, connection) {
     app.get('/getSession',function(req,res,next){
           res.send(req.session.users);
     });
+	
+    
+app.get('/auth/twitter', function(req, res) {
+ 
+  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+    if (error) {
+      res.redirect("/");
+    }
+    else {
+      req.session.oauth = {
+        token: oauth_token,
+        token_secret: oauth_token_secret
+      };
+    res.send('https://api.twitter.com/oauth/authenticate?oauth_token='+oauth_token);
+    }
+  });
+ 
+});
+    
+ app.get('/auth/twitter/callback', function(req, res, next) {
+ 
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth_data = req.session.oauth;
+    oauth.getOAuthAccessToken(
+      oauth_data.token,
+      oauth_data.token_secret,
+      oauth_data.verifier,
+      function(error, oauth_access_token, oauth_access_token_secret, results) {
+        if (error) {
+          res.send("Authentication Failure!");
+        }
+        else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          console.log(results);
+          var data={
+              id : results.user_id,
+    		  name : results.screen_name,
+    			email : '',
+    			img : '',
+          }
+          req.session.users = data;
+          res.redirect('/');
+        }
+      }
+    );
+  }
+  else {
+    res.redirect('/login'); // Redirect to login page
+  }
+ 
+});
 };
 
 
