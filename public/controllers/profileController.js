d7e1cdeee4e38107136b3c1e0044b5e463e83d6c
lingeswrn <lingeswrn@gmail.com>
2016-11-05 13:31:45 +0530

    Profile page
    Update

--- public/controllers/profileController.js
+++ public/controllers/profileController.js
@@ -1,24 +1,106 @@
-app.controller("profileController", function($scope, Session, $http){
-	$scope.data = {};
+app.controller("profileController", function($scope, Session, $http, $window){
+	$scope.data = {};	
 	$scope.session = Session;
-	
+	//$scope.arrow = true;
 	$scope.return = function(){
 		$().select2fn();
 	}
 	
-	if($scope.session.data == ""){
+	if($scope.session.data == ""){	
+		Session.updateSession();
+		$window.location.href = '#/';
 		$scope.hasuser="false";
 	}else{
 		$scope.hasuser="true";
 		$scope.username = $scope.session.data.first_name;
 	}
 	
+	/* Load loggedin User Profile */ 
+	$scope.loadData = function(){
+		$http.get('/my-profile').success(function(response){
+			$scope.profileData = response;
+		});
+		$http.get('/city').success(function(response){
+			$scope.city = response;
+		});
+		$http.get('/pincode').success(function(response){
+			$scope.pincode = response;
+		});
+		$http.get('/gnere').success(function(response){
+			$scope.gnere = response;
+		});
+	}
+	
+	/* Edit loggedin User Profile */ 
+	$scope.editProfile = function(){
+		$().removeDisabled();
+	}
+	
+	/* Update loggedin User Profile */ 
+	$scope.updateProfile = function(){
+		
+		console.log($scope.profileData)
+		/* $http.put('/updateProfile').success(function(response){
+			console.log($scope.profileData)
+			//$scope.gnere = response;
+		}); */
+	}
+	
 	setTimeout(function(){
 		$scope.return();
-	},1000);
+	},500);
 	
 	
 	$.fn.select2fn = function() {
-		$('select').select2();
+		$('#interestCategory').select2();
+		$("#derp").find('input').attr('disabled', 'true');
+		$("#derp").find('select').attr('disabled', 'true');
+	};
+	
+	$.fn.removeDisabled = function() {
+		$("#derp").find('input').removeAttr('disabled');
+		$("#derp").find('select').removeAttr('disabled');
+	};
+	$.fn.defaultSelect2Values = function() {
+			for(var i=0;i<tag.length;i++){
+				tags.push(tag[i].name);
+				tag_id.push(tag[i].id);
+			}
+		
+			 selections = ["37"];
+			
+			var extract_preselected_ids = function (element) {
+				var preselected_ids = [];
+				if (element.val())
+					$(element.val().split(",")).each(function () {
+						preselected_ids.push({id: this});
+					});
+				return preselected_ids;
+			};
+
+			var preselect = function (preselected_ids) {
+				var pre_selections = [];
+				for (index in selections)
+					for (id_index in preselected_ids)
+						if (selections[index].id == preselected_ids[id_index].id)
+							pre_selections.push(selections[index]);
+						console.log(pre_selections);
+				return pre_selections;
+			};
+			 $("#select2-option").select2();
+			$('#art_tag').select2({
+				placeholder: 'Select Tags',
+				minimumInputLength: 0,
+				multiple: true,
+				allowClear: true,
+				data: function () {
+					return {results: selections}
+				},
+				initSelection: function (element, callback) {
+					var preselected_ids = extract_preselected_ids(element);
+					var preselections = preselect(preselected_ids);
+					callback(preselections);
+				}
+			}).select2('val', tag_id);
 	};
 });
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,39 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-
-module.exports = router;
+module.exports = function(app, passport, connection) {
+	// GET USER PROFILE DETAILS
+    app.get('/my-profile', function(req, res) {
+		var email = req.session.users.email;		
+		var query = "SELECT users.*, user_address.* FROM `users`, user_address WHERE users.email_id='"+email+"' AND user_address.user_id = users.id";
+		connection.query(query, function(err, rows){
+			if(err) throw err;
+			res.send(rows[0]);
+		});		
+    });
+	
+	// GET CITY
+	app.get('/city', function(req, res) {	
+		var query = "SELECT * FROM `delivery_city` WHERE status = '1'";
+		connection.query(query, function(err, rows){
+			if(err) throw err;
+			res.send(rows);
+		});		
+    });
+	
+	// GET PINCODE
+	app.get('/pincode', function(req, res) {
+		var query = "SELECT `area` FROM `delivery_areas` WHERE status = '1'";
+		connection.query(query, function(err, rows){
+			if(err) throw err;
+			res.send(rows);
+		});		
+    });
+	
+	// GET GNERE 
+	app.get('/gnere', function(req, res) {
+		var email = req.session.users.email;
+		var query = "SELECT `id`, `category_name` as name FROM `category` WHERE deleted = '0'";
+		connection.query(query, function(err, rows){
+			if(err) throw err;
+			res.send(rows);
+		});		
+    });
+}
